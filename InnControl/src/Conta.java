d342a4460181e2411494cdb8a8e2ff561e023ce2
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2023-11-13 01:45:55 -0300

    Update

---
+++ InnControl/src/Conta.java
@@ -0,0 +1,30 @@
+public class Conta
+{
+    private int numeroDaConta;
+    private String nomeDoHospede;
+
+    public Conta()
+    {
+        
+    }
+
+    public int getNumeroDaConta()
+    {
+        return numeroDaConta;
+    }
+
+    public void setNumeroDaConta(int numeroDaConta)
+    {
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public String getNomeDoHospede()
+    {
+        return nomeDoHospede;
+    }
+
+    public void setNomeDoHospede(String nomeDoHospede)
+    {
+        this.nomeDoHospede = nomeDoHospede;
+    }    
+}
