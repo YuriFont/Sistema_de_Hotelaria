0ef8873dd9bb59ceaf61c4a81ebdd8163cf59866
Miguel Moura <miguelmouralob@gmail.com>
2023-11-13 17:00:31 -0300

    Update

--- InnControl/src/Armazem.java
+++ InnControl/src/Armazem.java
@@ -1,3 +1,20 @@
 public class Armazem {
-    
+    private String produtos;
+    private int quantidade;
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getProdutos() {
+        return this.produtos;
+    }
+
+    public void setProdutos(String produtos) {
+        this.produtos = produtos;
+    }
 }
