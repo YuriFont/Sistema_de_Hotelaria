cc36cfa580c42c6d35f0ebd3dd5ac1d033eb5174
Felipe Macedo Kelner <felipe.cmacedo00@gmail.com>
2023-11-16 14:47:33 -0300

    Update

--- InnControl/src/Recepcionista.java
+++ InnControl/src/Recepcionista.java
@@ -9,20 +9,20 @@ public class Recepcionista extends Funcionario
         try (FileWriter fileWriter = new FileWriter("InnControl/registros/registros_reserva.txt", true);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) { 
 
-            // Formato: Nome - ID - Telefone
+            
             String registro = nome + " - " + id + " - " + telefone;
 
-            // Escrever no arquivo
+            // Escreve no arquivo
             bufferedWriter.write(registro);
-            bufferedWriter.newLine(); // Adicionar uma nova linha para separar registros
+            bufferedWriter.newLine(); // Adiciona uma nova linha para separar registros
 
             System.out.println("Reserva realizada com sucesso!");
 
-            return true; // Indicar que a reserva foi realizada com sucesso
+            return true; 
 
         } catch (IOException e) {
             System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
-            return false; // Indicar que ocorreu um erro ao escrever no arquivo
+            return false; 
         }
     }
 }
