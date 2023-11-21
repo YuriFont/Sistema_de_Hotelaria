3e0bf6d8f11646e1ca894361ef589653ebd382ec
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2023-11-21 13:24:42 -0300

    Update

--- InnControl/src/Main.java
+++ InnControl/src/Main.java
@@ -5,22 +5,22 @@
  * YURI FONTENELE SILVA (1220105046)
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main 
 {
     public static void main(String[] args) throws Exception 
     {
-        Hospede hospede1 = new Hospede("Joao", 1, "123456789");
-        hospede1.realizarReserva();
-        Hospede hospede2 = new Hospede("Maria", 2, "987654321");
-        hospede2.realizarReserva();
+        List<Hospede> list = new ArrayList<>();
 
-        Conta conta1 = new Conta(1, "Joao", hospede1);
+        /*Conta conta1 = new Conta(1, "Joao", hospede1);
         Conta conta2 = new Conta(2, "Maria", hospede2);
 
         Arrumadeira arrumadeira = new Arrumadeira();
         conta1.setValor(arrumadeira.registrarConsumo());
         arrumadeira.fecharScanner();
 
-        hospede2.pagarConta(conta2, hospede2);
+        hospede2.pagarConta(conta2, hospede2);*/
     }
 }
