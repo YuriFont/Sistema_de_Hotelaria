4eba8b18592974a42fdf2d92faaaaf5178d30b6a
Felipe Macedo Kelner <felipe.cmacedo00@gmail.com>
2023-11-21 20:26:40 -0300

    Update

--- InnControl/src/Main.java
+++ InnControl/src/Main.java
@@ -1,10 +1,15 @@
+/*
+ * FELIPE MACEDO KELNER (1220106097)
+ * MIGUEL DE MOURA LOBATO (1220109077)
+ * RAFAEL CEZAR COELHO DA MOTA (1220103200)
+ * YURI FONTENELE SILVA (1220105046)
+ */
+
 public class Main 
 {
     public static void main(String[] args) 
     {
         Recepcionista recepcionista = new Recepcionista();
-
         recepcionista.fazerCheckIn();
-        
     }
 }
